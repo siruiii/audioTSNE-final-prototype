1e93f47af4254306e50731705f19038a962f8400
siruiii <144714732+siruiii@users.noreply.github.com>
2024-08-17 23:16:30 -0400

    Remove K-mean Clustering

--- 3-Mapping/3.1-coordinates.py
+++ 3-Mapping/3.1-coordinates.py
@@ -4,7 +4,7 @@ import math
 # Function to process the JSON data
 def process_json_data(data):
     # Compute the min and max for normalization
-    points = [item['point'][0]*13 for item in data]
+    points = [item['point'][0]*1.3 for item in data]
     max_x = max(points)
     min_x = min(points)
     range_x = max_x - min_x
@@ -14,8 +14,8 @@ def process_json_data(data):
     # Add the 3d field to each item
     for item in data:
         point = item['point']
-        x2d = point[0]*13
-        y3d = point[1]*13
+        x2d = point[0]*1.3
+        y3d = point[1]*1.3
         theta = ((x2d - min_x) / range_x) * 2 * math.pi
         x3d = radius * math.cos(theta)
         z3d = radius * math.sin(theta)
@@ -24,7 +24,7 @@ def process_json_data(data):
     return data
 
 # Read JSON data from a file
-input_file = '../2-DBSCAN-KMEAN/peking25-5sec_tsne_results_with_clusters(DBSCAN with KMEAN).json'  # Replace with your input file path
+input_file = '../2-DBSCAN-KMEAN/peking25-10sec_tsne_results_with_clusters(DBSCAN).json'  # Replace with your input file path
 output_file = '_coordinates.json'  # Replace with your output file path
 
 with open(input_file, 'r') as file:
